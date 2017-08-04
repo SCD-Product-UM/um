7255b233449fb3407d3317695e823fe1ee488f06
jinhui.lu <jinhui.lu@tcl.com>
2017-08-04 13:52:08 +0800

    修改cpe UM维护方案，提高效率，避免频繁维护

---
+++ auto.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python updateCpe.py
+python updateOld.py
+#read
---
+++ cpe_config.js
@@ -0,0 +1,77 @@
+var cpe_config = {
+  "HH40": {
+    "00_Generic": {
+      "umlist": [
+        "USER_Manual_de.pdf", 
+        "USER_Manual_en.pdf", 
+        "USER_Manual_es.pdf", 
+        "USER_Manual_fr.pdf", 
+        "USER_Manual_it.pdf", 
+        "USER_Manual_tr.pdf"
+      ]
+    }, 
+    "E4_ALAT": {
+      "umlist": [
+        "USER_Manual_de.pdf", 
+        "USER_Manual_en.pdf", 
+        "USER_Manual_es.pdf", 
+        "USER_Manual_fr.pdf", 
+        "USER_Manual_it.pdf", 
+        "USER_Manual_tr.pdf"
+      ]
+    }, 
+    "规则说明.txt": {}
+  }, 
+  "HH41": {
+    "00_Generic": {
+      "umlist": [
+        "USER_Manual_ar.pdf", 
+        "USER_Manual_bg.pdf", 
+        "USER_Manual_de.pdf", 
+        "USER_Manual_en.pdf", 
+        "USER_Manual_es.pdf", 
+        "USER_Manual_fr.pdf", 
+        "USER_Manual_hr.pdf", 
+        "USER_Manual_it.pdf", 
+        "USER_Manual_mk.pdf", 
+        "USER_Manual_sl.pdf", 
+        "USER_Manual_sr.pdf"
+      ]
+    }, 
+    "01_Generic_Latam": {
+      "umlist": [
+        "USER_Manual_en.pdf", 
+        "USER_Manual_es.pdf"
+      ]
+    }, 
+    "JO_claro_dominica": {
+      "umlist": [
+        "USER_Manual_en.pdf", 
+        "USER_Manual_es.pdf"
+      ]
+    }, 
+    "KR_Orange_Tunisia": {
+      "umlist": [
+        "flybox_um_ar.pdf", 
+        "flybox_um_en.pdf", 
+        "flybox_um_fr.pdf"
+      ]
+    }, 
+    "规则说明.txt": {}
+  }, 
+  "HH70": {
+    "00_Generic": {
+      "umlist": [
+        "USER_Manual_ar.pdf", 
+        "USER_Manual_de.pdf", 
+        "USER_Manual_en.pdf", 
+        "USER_Manual_es.pdf", 
+        "USER_Manual_fr.pdf", 
+        "USER_Manual_hr.pdf", 
+        "USER_Manual_it.pdf", 
+        "USER_Manual_pl.pdf"
+      ]
+    }, 
+    "规则说明.txt": {}
+  }
+}
